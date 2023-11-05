a6c51f72b0029aca39b93cfa9d58c5e1bf3a97c2
hager-dot <hagerelhwarii6633@gmail.com>
2023-11-05 11:17:00 +0200

    :sparkles: Toggle between play and pause buttons

--- 04. Stop Watch/README.md
+++ 04. Stop Watch/README.md
@@ -1,4 +1,4 @@
 # Stop Watch :bulb:
-- Simple `Stop Watch` that have the Stop, and Reset functionality. using HTML, CSS and `JavaScript`.
+- Simple `Stop Watch` that have the Start, Pause, and Reset functionality. using HTML, CSS and `JavaScript`.
 
 ## :camera_flash: Demo Screenshoot
--- 04. Stop Watch/script.js
+++ 04. Stop Watch/script.js
@@ -2,10 +2,12 @@ let timer = document.getElementById("timer");
 let startBtn = document.querySelector("#play-btn");
 let resetBtn = document.querySelector("#reset-btn");
 
+// Variables for timer values.
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+// Variables for leading zeros.
 let leadingSeconds = 0;
 let leadingMinutes = 0;
 let leadingHours = 0;
@@ -13,6 +15,7 @@ let leadingHours = 0;
 let timerInterval = null;
 let timerStatus = "stopped";
 
+// Start  timer function.
 const startWatch = () => {
   seconds++;
   if (seconds / 60 === 1) {
@@ -43,18 +46,28 @@ const startWatch = () => {
   timer.innerText = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`;
 };
 
+// Event Listener on user click either, on play or pause button.
 startBtn.addEventListener("click", () => {
-  timerInterval = window.setInterval(startWatch, 1000);
-  // startBtn.classList.add ='pause-btn'
-  startBtn.innerHTML =
-    '<iconify-icon icon="iconoir:pause-solid"></iconify-icon>';
+  if (timerStatus === "stopped") {
+    timerInterval = window.setInterval(startWatch, 800);
+    startBtn.innerHTML =
+      '<iconify-icon icon="iconoir:pause-solid"></iconify-icon>';
+    timerStatus = "started";
+  } else {
+    window.clearInterval(timerInterval);
+    startBtn.innerHTML =
+      '<iconify-icon icon="iconoir:play-solid"></iconify-icon>';
+    timerStatus = "stopped";
+  }
 });
 
+// Event Listener that clear timerInterval and rest the timer.
 resetBtn.addEventListener("click", () => {
   clearInterval(timerInterval);
   seconds = 0;
   minutes = 0;
   hours = 0;
   timer.innerText = "00:00:00";
+  startBtn.innerHTML =
+      '<iconify-icon icon="iconoir:play-solid"></iconify-icon>';
 });
-
